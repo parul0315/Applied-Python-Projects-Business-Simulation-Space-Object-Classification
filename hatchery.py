1f5e04dfb5b3a7c6fd5713e34743171ebc126b90
parul0315 <parulnagar1245@gmail.com>
2024-11-08 21:23:24 +0000

    Define the class Hatchery, with other changes.

---
+++ hatchery.py
@@ -0,0 +1,10 @@
+from fish import Fish
+from technician import Technician
+from supplier import Supplier
+from warehouse import Warehouse
+
+class hatchery:
+    """
+    Hatchery class contains attributes and associated functions about the supplies, cash status, and number of technicians.
+    """
+    def __init__(self, cash = 10000):
