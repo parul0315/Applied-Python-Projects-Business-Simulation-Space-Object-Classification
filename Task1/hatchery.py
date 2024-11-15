e9f4594a0b12b885d24751bb999fbb1e730b70ac
parul0315 <parulnagar1245@gmail.com>
2024-11-15 05:35:35 +0000

    created a method for selling fish

--- Task1/hatchery.py
+++ Task1/hatchery.py
@@ -3,7 +3,7 @@ from technician import Technician
 from supplier import Supplier
 from warehouse import Warehouse
 
-class hatchery:
+class Hatchery:
     """
     Hatchery class contains attributes and associated functions about the supplies, cash status, and number of technicians.
     """
@@ -18,4 +18,63 @@ class hatchery:
             "Fugue Flounder": Fish("Fugue Flounder", 200.0, 12, 2, 2.5, 30, 550),
             "Modal Bass": Fish("Modal Bass", 300.0, 12, 6, 3.0, 50, 500)
         }
-    
+        self.warehouse = [
+            Warehouse("fertilizers", 20, 10, 0.4, 0.1),
+            Warehouse("feed", 400, 200, 0.1, 1),
+            Warehouse("salt", 200, 100, 0.1, 1)
+        ]
+        self.suppliers = [
+            Supplier("Slippery Lakes", 0.30, 0.10, 0.05),
+            Supplier("Scaly Wholesaler", 0.20, 0.40, 0.25)
+        ]
+
+        def add_technician(self, name, quarter, specialty=None):
+            if len(self.technicians) < 5:
+                new_technician = Technician(name, specialty=specialty)
+                self.technicians.append(new_technician)
+                print("Hired {self.technician[-1].name}, weekly rate = 500, for quarter {quarter}")
+                return True
+            return "We already have the max amount of Technicians"
+
+        def remove_technician(self):
+            if len(self.technicians) < 0:
+                print("Let go of {self.technician[-1].name}")
+                self.technicians.pop()
+            return "No technicians available to let go"
+        
+        def sell_fish(self, fish_type, quantity):
+            if fish_type in self.fish_types:
+                fish = self.fish_types[fish_type]
+            
+                # Limit the quantity to the demand if it exceeds
+                actual_quantity = min(quantity, fish.demand)
+                total_fertilizer = fish.fertilizer * quantity
+                total_feed = fish.feed * quantity
+                total_salt = fish.salt * quantity
+
+                if (self.warehouse[0].main + self.warehouse[0].auxiliary >= total_fertilizer and
+                    self.warehouse[1].main + self.warehouse[1].auxiliary >= total_feed and 
+                    self.warehouse[2].main + self.warehouse[2].auxiliary >= total_salt):
+
+                    #check if we have enough labour
+                    total_time = len(self.technicians) * 9 * 7
+                    required_labour = fish.time * quantity
+                    if total_time >= required_labour:
+                        self.cash += fish.price * quantity
+                        self.use_supplies(total_fertilizer, total_feed, total_salt)
+                        return f"Sold {quantity} {fish_type} for {fish.price * quantity} pounds"
+                    else:
+                        return f"do not have enough labour to maintain the amount of fish required"
+                else:
+                    return f"do not have enough supplies for the amount of fish required"
+                    
+            else:
+                return f"Invalid fish type"
+        
+        
+
+        def __str__(self):
+            return f"{self.cash}\n{self.fish_types}\n{self.warehouse}\n{self.suppliers}"
+
+hatch = Hatchery()
+print(hatch)
